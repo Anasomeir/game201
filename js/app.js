a7a43a6ace6fff6e8cc3345a662c4bb0f3de464d
Anasomeir <anas.jawad.omair@gmail.com>
2021-03-17 14:59:47 +0200

    Game about me

---
+++ js/app.js
@@ -0,0 +1,127 @@
+
+'use strict';
+let yname= prompt('what is your name ?');
+alert('Welcome '+yname);
+
+let Score=0 ;
+let answer=prompt('Do you think that I am Living in Irbed?');
+  if (answer.toLowerCase()==='no'|| answer.toLowerCase() ==='n') {
+    Score++;
+    console.log('true');
+    alert('true');
+    alert('your answer is '+answer+' ,  yes I am living in the Capital');
+  } else {
+    console.log('false');
+    alert('false');
+    alert('you answer is '+answer + ' NO IAM NOT ');
+  }
+
+  let answer2=prompt('Do you think that I can Swim?');
+  if (answer2.toLowerCase()==='no'|| answer2.toLowerCase() ==='n') {
+    Score++;
+    console.log('true');
+    alert('true');
+    alert('your answer is '+answer2+'  You are right Unfortunately I am not good swimmer');
+  } else {
+    console.log('false');
+    alert('false');
+    alert('you answer is '+answer2 + ' Nope, Unfortunately I am not good swimmer ');
+  }
+
+  let answer3=prompt('Do you think that I Love Football?');
+  if (answer3.toLowerCase()==='yes'|| answer3.toLowerCase() ==='y') {
+    Score++;
+    console.log('true');
+    alert('true');
+    alert('your answer is '+answer3+'  You are right Unfortunately I am not good swimmer');
+  } else {
+    console.log('false');
+    alert('false');
+    alert('you answer is '+answer3 + ' Nope, Unfortunately I am not good swimmer ');
+  }
+
+  let answer4=prompt('Do you think that I have beard?');
+  if (answer4.toLowerCase()==='yes'|| answer4.toLowerCase() ==='y') {
+    Score++;
+    console.log('true');
+    alert('true');
+    alert('your answer is '+answer4+'  You are right I have it But iam not Shiakh');
+  } else {
+    console.log('false');
+    alert('false');
+    alert('you answer is '+answer4 + ' Actually, I have a beard But Im not Shaikh ');
+  }
+
+
+  let answer5=prompt('Do you think that I can drive a truck?');
+  if (answer5.toLowerCase()==='no'|| answer5.toLowerCase() ==='n') {
+    Score++;
+    console.log('true');
+    alert('true');
+    alert('your answer is '+answer5+'  You are right I cant drive it ');
+  } else {
+    console.log('false');
+    alert('false');
+    alert('you answer is '+answer5 + ' Nope, Unfortunately I cant drive it ');
+  }
+
+
+  let answer6;
+  // eslint-disable-next-line no-case-declarations
+  let i=0;
+  do {
+    i=i++;
+    answer6=Number(prompt('How old Do you think I am ? '));
+    if (answer6>32) {
+      console.log('You have guessed too high!');
+      alert('You have guessed too high!');
+      // eslint-disable-next-line no-unused-vars
+      alert ('again') ;
+    }
+    else if (answer6===32) {
+      console.log('true');
+      alert('true');
+      point++;
+    }
+    else {
+      console.log('You have guessed too low!');
+      alert('You have guessed too low!');
+      // eslint-disable-next-line no-unused-vars
+      alert ('again') ;
+    }
+    console.log(i);
+    i++;
+  } while ( answer6 !== 23 && i<4);
+  alert('I am 32 years old');
+
+
+
+  let Place =['Capital','Jordan','Amman'];
+  // eslint-disable-next-line no-case-declarations
+  let C=0;
+  // eslint-disable-next-line no-case-declarations
+  let answer7;
+  do{
+    //k++;
+    answer7=prompt('Guess where I am living');
+    if (answer7===h[0]) {
+      alert(answer7 +'  true');
+      k=6;
+      point++;
+    }
+    else if (answer7===h[1]) {
+      alert(answer7 +' True');
+      k=6;
+      point++;
+    }
+    else if (answer7===h[2]) {
+      alert(answer7 +'  true');
+      k=6;
+      point++;
+    }
+    console.log(k);
+    k++;
+  }while( k!== 6 );
+  alert('I live in'+h);
+
+//hello howare 
